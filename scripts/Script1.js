a096e7c1d67c3f6405dc3df6e2957bc9a57ab8fb
Theophile0 <theophile.vandesteene@student.vives.be>
2023-07-10 09:16:52 +0200

    Setup en eerste principes van project

--- scripts/Script1.js
+++ scripts/Script1.js
@@ -28,7 +28,7 @@ const overloop = () => {
   input.setAttribute("name", "Text",);
   input.setAttribute("id", "input");
   input.setAttribute("placeholder", "Vul hier een naam in");
-    button.setAttribute("id", "")
+  button.setAttribute("id", "");
   div.appendChild(input);
 }
 
