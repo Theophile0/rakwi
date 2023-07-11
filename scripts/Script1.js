4dcac2bd28d576f7c7520fc53eba3cdf535ba4e0
Théophile0 <theophile.vandesteene@student.vives.be>
2023-07-11 09:09:21 +0200

    Probleem met images proberen oplossen

--- scripts/Script1.js
+++ scripts/Script1.js
@@ -18,20 +18,31 @@ const setup = () => {
     controleerbtn = document.getElementById("controleer");
 
 }
-const overloop = () => {
-  let div = document.getElementById("divke");
+const controleer = () => {
+    let naam = document.getElementById("inputText");
+    for (let i = 0; i < namen.length; i++) {
+        if (namen[i].naam === naam){
+            if(namen[i].rood === true){
 
-  let input = document.createElement("input");
-  let button = document.createElement("button");
+            }
+        }
+    }
 
-  input.setAttribute("type", "text");
-  input.setAttribute("name", "Text",);
-  input.setAttribute("id", "input");
-  input.setAttribute("placeholder", "Vul hier een naam in");
-  button.setAttribute("id", "controleer");
-  button.setAttribute("placeholder", "Controleer")
-  div.appendChild(input);
-  div.appendChild(button);
+}
+const terugOrigineel = () => {
+    let div = document.getElementById("divke");
+
+    let input = document.createElement("input");
+    let button = document.createElement("button");
+
+    input.setAttribute("type", "text");
+    input.setAttribute("name", "Text",);
+    input.setAttribute("id", "input");
+    input.setAttribute("placeholder", "Vul hier een naam in");
+    button.setAttribute("id", "controleer");
+    button.setAttribute("placeholder", "Controleer")
+    div.appendChild(input);
+    div.appendChild(button);
 }
 
 window.addEventListener("load", setup);
