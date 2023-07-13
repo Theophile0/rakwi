c9790665cecc53514ec60e48bf97d0ed32b04851
Théophile0 <theophile.vandesteene@student.vives.be>
2023-07-13 16:30:03 +0200

    Laatste puntjes op de i nu alleen nog css

--- scripts/Script1.js
+++ scripts/Script1.js
@@ -24,7 +24,7 @@ const controleer = () => {
     for (let i = 0; i < namen.length; i++) {
         if (namen[i].naam === name){
             if(namen[i].rood === true){
-                body[0].style.backgroundImage = "url(\"../images/images.jpg\")";
+                body[0].style.backgroundImage = "url(\"../images/rood.png\")";
                 hideInput();
                 let naam = document.createElement("div");
                 naam.appendChild(document.createTextNode(name));
@@ -32,16 +32,21 @@ const controleer = () => {
                 body[0].appendChild(naam);
             }
             else{
-
+                body[0].style.backgroundImage = "url(\"../images/groen.jpg\")";
+                hideInput();
+                let naam = document.createElement("div");
+                naam.appendChild(document.createTextNode(name));
+                naam.setAttribute("id", "naam");
+                body[0].appendChild(naam);
             }
         }
     }
 
 }
 const hideInput = () => {
-    let input = document.createElement("input");
-    let button = document.createElement("button");
-    input.setAttribute("type", "hidden")
+    let input = document.getElementById("inputText");
+    let button = document.getElementById("controleer");
+    input.style.display = "none";
     button.style.display = "none";
 }
 const terugOrigineel = () => {
